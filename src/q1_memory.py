a0bd621656746dfdf5aae13823de994185f9756d
Antoni Yupanqui <antony2919@gmail.com>
2024-03-17 13:15:43 -0500

    Recorrido en la lista de tweets y captura de datos del conteo de pub por fecha y pub por usuario y fecha

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -12,3 +12,14 @@ def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
         lineas = contenido.readlines()  #type lineas = class 'list' y contiene \n de cada linea
     lista_tweets = [json.loads(linea.rstrip()) for linea in lineas] #recorremos la lista contenido y quitamos los espacios de la derecha que pueda tener la linea y la incluimos dentro de una lista, con esto evitamos los \n de cada linea
 
+    #Recorremos la lista de tweets
+    for tweet in lista_tweets:
+        #Obtener el contenido del diccionario con clave date
+        fecha_str = tweet.get("date")
+        #Convertir el GMT al pais que desee analizar o mantener en GMT = 0 como viene por defecto
+        fecha_hora = datetime.fromisoformat(fecha_str)
+        nueva_fecha_hora = fecha_hora + timedelta(hours=-5) #en nuestro modificamos al gmt de peru -5,o de acuerdo a lo requerido analizar.
+        fecha_formateada = nueva_fecha_hora.strftime("datetime.date(%Y, %m, %d)") #Formateamos la fecha al formato indicado
+        pub_por_fecha[fecha_formateada] += 1
+        pub_usuario_fecha[fecha_formateada][tweet['user']['username']] +=1
+
